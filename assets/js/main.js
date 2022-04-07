9ca4ca1a3298c55ca73b294f68ccd58d6fceae5f
satoru komatsu <sk.comiu@gmail.com>
2022-04-07 13:29:13 +0900

    ハンバーガーメニューの改修

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
+const body = document.body;
+const head = document.querySelector(".header");
 const nav = document.querySelector("#js-nav");
 const ham = document.querySelector("#js-hamburger");
 
-ham.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  ham.classList.toggle("active");
-});
+//ハンバーガーメニューのタップした際のクラス付与とiOSのスクロール禁止用関数
+window.onload = function () {
+  ham.addEventListener("click", () => {
+    body.classList.toggle("active");
+    head.classList.toggle("active");
+    nav.classList.toggle("active");
+    ham.classList.toggle("active");
+
+    function disableScroll(e) {
+      e.preventDefault();
+    }
+
+    document.addEventListener("touchmove", disableScroll);
+  });
+};
